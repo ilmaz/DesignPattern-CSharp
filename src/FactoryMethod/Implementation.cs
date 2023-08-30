d0ea293248180773e4e2a52af5804dfc168f8f39
Ilmaz Sourani <i.sourani@mobinnet.ir>
2023-08-30 15:29:12 +0330

    Session 1

---
+++ src/FactoryMethod/Implementation.cs
@@ -0,0 +1,89 @@
+﻿namespace FactoryMethod
+{
+    /// <summary>
+    /// Product
+    /// </summary>
+    public abstract class DiscountService
+    {
+        public abstract int DiscountPercentage { get; }
+        public override string ToString() => GetType().Name;
+    }
+
+    /// <summary>
+    /// ConcreteProduct
+    /// </summary>
+    public class CountryDiscountService : DiscountService
+    {
+        private readonly string _countryIdentifier;
+
+        public CountryDiscountService(string countryIdentifier) => _countryIdentifier = countryIdentifier;
+
+        public override int DiscountPercentage
+        {
+            get
+            {
+                switch (_countryIdentifier)
+                {
+                    case "IR":
+                        return 20;
+                    default: return 10;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// ConcreteProduct
+    /// </summary>
+    public class CodeDiscountService : DiscountService
+    {
+        private readonly Guid _code;
+
+        public CodeDiscountService(Guid code) => _code = code;
+
+        public override int DiscountPercentage => 15;
+    }
+
+    /// <summary>
+    /// Creator
+    /// </summary>
+    public abstract class DiscountFactory
+    {
+        public abstract DiscountService CreateDiscountService();
+    }
+
+    /// <summary>
+    /// ConcreteCreator
+    /// </summary>
+    public class CountryDiscountFactory : DiscountFactory
+    {
+        private readonly string _countryIdentifier;
+
+        public CountryDiscountFactory(string countryIdentifier) => _countryIdentifier = countryIdentifier;
+
+        public override DiscountService CreateDiscountService()
+        {
+            return new CountryDiscountService(_countryIdentifier);
+        }
+            
+    }
+
+    /// <summary>
+    /// ConcreteCreator
+    /// </summary>
+    public class CodeDiscountFactory : DiscountFactory
+    {
+        private readonly Guid _code;
+
+        public CodeDiscountFactory(Guid code) => _code = code;
+
+        public override DiscountService CreateDiscountService()
+        {
+            return new CodeDiscountService(_code);
+        }
+    }
+
+}
+
+
+
---
+++ src/Singleton/Implementation.cs
@@ -0,0 +1,42 @@
+﻿namespace Singleton
+{
+    public class Logger
+    {
+        private static Logger? _instance;
+
+        public static Logger Instance
+        {
+            get
+            {
+
+                if (_instance == null)
+                {
+                    _instance = new Logger();
+                }
+                return _instance;
+            }
+        }
+
+        protected Logger() { }
+
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class LazyLogger
+    {
+        private static readonly Lazy<LazyLogger> _lazyLogger = new(() => new LazyLogger());
+
+        public static LazyLogger Instance => _lazyLogger.Value;
+
+        protected LazyLogger() { }
+
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
+
